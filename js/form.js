f81d363a5aef57f6c8004e48a450ac995aa57fac
Alina Glebova <alina.lina2407@gmail.com>
2020-07-05 18:02:03 +0300

    Добавила фильтры в каталоге, анимацию и валидацию формы

---
+++ js/form.js
@@ -0,0 +1,34 @@
+var searchButton = document.querySelector('.show-modal-button');
+var searchForm = document.querySelector('.search-form');
+var arrivalDate = document.querySelector('#arrival-date');
+var departureDate = document.querySelector('#departure-date');
+var adult = document.querySelector('#adult');
+var children = document.querySelector('#children');
+
+searchButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (searchForm.classList.contains('modal-show')) {
+    searchForm.classList.remove('modal-show');
+    searchForm.classList.add('modal-hide');
+  } else {
+    searchForm.classList.remove('modal-hide');
+    searchForm.classList.add('modal-show');
+  }
+});
+
+searchForm.addEventListener('submit', function (evt) {
+  if (!arrivalDate.value || !departureDate.value || !adult.value) {
+    evt.preventDefault();
+    searchForm.classList.add('modal-error');
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (searchForm.classList.contains("modal-show")) {
+      evt.preventDefault();
+      searchForm.classList.remove("modal-show");
+      searchForm.classList.remove("modal-error");
+    }
+  }
+});
